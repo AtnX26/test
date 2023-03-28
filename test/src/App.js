f30765c2a2d2e8bae4c1a39416d11b19f9d03d96
AtnX26 <atnxdh26@gmail.com>
2023-03-27 22:54:13 -0400

    implementing the UI with simple logic

--- test/src/App.js
+++ test/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let message = ''
+
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs);
+    message = inputs.name
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form onSubmit={handleSubmit}>
+      <label>Enter your name:
+      <input 
+        maxLength={150}
+        type="text" 
+        name="username" 
+        value={inputs.username || ""} 
+        onChange={handleChange}
+      />
+      </label>
+      <br />
+      <label>Enter your age:
+      <input 
+        maxLength={125}
+        type="text" 
+        name="age" 
+        value={inputs.age || ""} 
+        onChange={handleChange}
+      />
+      </label>
+        <br />
+      <label>
+      Favorite season:
+      <select name="selectSeason">
+        <option value="Spring">Spring</option>
+        <option value="Summer">Summer</option>
+        <option value="Fall">Fall</option>
+        <option value='Winter'>Winter</option>
+      </select>
+      </label>
+      <p>
+        Can you drive a car?
+        <label>
+          <input type="radio" 
+                  name="myRadio" 
+                  value="option1" />
+          Yes
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="myRadio"
+            value="option2" 
+          />
+          No
+        </label>
+      </p>
+      <label>{message}</label>
+        <input type="submit" />
+    </form>
+  )
+  
 }
 
 export default App;
